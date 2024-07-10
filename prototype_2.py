e2d98c051b4369b121fd191e2fa32a8af4367c5b
Michael Kamensky <michaelkamensky03@gmail.com>
2024-07-10 14:17:29 -0700

    adding the file creation aspect of the program

--- prototype_2.py
+++ prototype_2.py
@@ -4,15 +4,75 @@ import re # needed parsing using regular expressions
 import subprocess # needed for reading the terminal so we can stop the process
 from collections import defaultdict
 
+class TreeNode:
+    def __init__(self, node_id, time):
+        self.id = node_id
+        self.time = time
+        self.children = []
+        self.parent = None
+
+    def add_child(self, child_node):
+        self.children.append(child_node)
+        child_node.parent = self
+
+    def get_sorted_children(self):
+        # Sort children based on the time attribute, youngest first
+        return sorted(self.children, key=lambda x: x.time)
+
+
+class Tree:
+    def __init__(self):
+        self.nodes = {}
+        self.root = None
+
+    def add_node(self, node_id, parent_id, time):
+        if node_id in self.nodes:
+            raise ValueError(f"Node with id {node_id} already exists.")
+        
+        new_node = TreeNode(node_id, time)
+        self.nodes[node_id] = new_node
+
+        if parent_id is None:
+            self.root = new_node
+        else:
+            parent_node = self.nodes.get(parent_id)
+            if parent_node is None:
+                raise ValueError(f"Parent with id {parent_id} does not exist.")
+            parent_node.add_child(new_node)
+            # Update root if needed
+            if self.root is None:
+                self.root = parent_node
+
+    def in_order_traversal(self, node=None, result=None):
+        if node is None:
+            node = self.root
+        if result is None:
+            result = []
+
+        if not node:
+            return result
+
+        sorted_children = node.get_sorted_children()
+
+        if sorted_children:
+            self.in_order_traversal(sorted_children[0], result)
+        
+        result.append((node.id, node.time))
+        
+        for child in sorted_children[1:]:
+            self.in_order_traversal(child, result)
+
+        return result
+
 # now we need a lines class to keep track of each line there meta deta
 
 class line():
-    def __init__(self, id ,line):
+    def __init__(self, id ,my_line):
         self.id = id
-        self.type = "none"
-        self.line = line
+        self.type = "None"
+        self.line = my_line
         self.attributes = defaultdict(list)
-        self.parse_string_to_dict(line)
+        self.parse_string_to_dict(my_line)
     
     def __str__(self):
         return self.line
@@ -99,22 +159,19 @@ def main():
     parser.add_argument('-o', '--outfile', dest='out', required=True, metavar='FILE')
     #parser.add_argument('filename')           # positional argument
     args = parser.parse_args()
-    print(args.audit)
-    print(args.out)
+    #print(args.audit)
+    #print(args.out)
     a = AuditLog(args.audit)
-    #for m in a.messages:
-        #message = a.messages[m]
-        #print("new message \n\n\n")
-        #for l in message.lines:
-            #print(l)
-    m100 = a.messages[100] # AuditLogMessage
-    print(m100.id)
-    for p in m100.lines: # AuditLogLine
-        print(p.type)
-        print(p.attributes)
-        if p.type == "EXECVE":
-            if "argc" in p.attributes:
-                print(p.attributes["argc"])
+    tree = Tree()
+
+    # now using this structure and a tree we will parse the audit log while also using a tree
+    for m in a.messages:
+        message = a.messages[m]
+        for line in message.lines:
+            if line.type == "SYSCALL" or line.type == "EXECVE":
+                print(line)
+
+
 
 
 
